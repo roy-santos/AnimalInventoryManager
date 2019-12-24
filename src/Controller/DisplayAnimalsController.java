eccfb503b71df75e102c0c56534c07a2a6bc2e8e
roy-santos <roysantos112@gmail.com>
2019-12-23 23:00:58 -0800

    Add update and delete methods.

--- src/Controller/DisplayAnimalsController.java
+++ src/Controller/DisplayAnimalsController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Model.Animal;
 import Model.DataProvider;
+import Model.Dog;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -15,6 +16,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
+import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -61,9 +63,34 @@ public class DisplayAnimalsController implements Initializable {
 
     public boolean search(int id) {
 
-        for(Animal dog: DataProvider.getAllAnimals()) {
+        for(Animal dog : DataProvider.getAllAnimals()) {
             if (dog.getId() == id) {
                 return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean update(int id, Animal animal) {
+
+        int index = -1;
+
+        for(Animal dog : DataProvider.getAllAnimals()) {
+            index++;
+
+            if(dog.getId() == id) {
+                DataProvider.getAllAnimals().set(index, animal);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean delete(int id) {
+
+        for(Animal dog : DataProvider.getAllAnimals()) {
+            if(dog.getId() == id) {
+                return DataProvider.getAllAnimals().remove(dog);
             }
         }
         return false;
@@ -80,10 +107,25 @@ public class DisplayAnimalsController implements Initializable {
         lifespanCol.setCellValueFactory(new PropertyValueFactory<>("lifespan"));
         priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
 
+        /*
         if(search(14)) {
             System.out.println("Match!");
         } else {
             System.out.println("No Match!");
+        }
+
+
+        if(update(66, new Dog(5, "German Shepard", 13, "Alert", 1599.99, true, "Drools"))) {
+            System.out.println("Update successful!");
+        } else {
+            System.out.println("Update failed!");
+        }
+         */
+
+        if(delete(3)) {
+            System.out.println("Delete successful!");
+        } else {
+            System.out.println("Delete failed!");
         }
     }
 
