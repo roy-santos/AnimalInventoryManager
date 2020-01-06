d4d11952e5a1e8569e251e5055fe6f902df14b4a
roy-santos <roysantos112@gmail.com>
2020-01-05 21:27:13 -0800

    Added exception handling and alerts.

--- src/Controller/CreateAnimalMenuController.java
+++ src/Controller/CreateAnimalMenuController.java
@@ -8,13 +8,16 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class CreateAnimalMenuController implements Initializable {
@@ -46,36 +49,50 @@ public class CreateAnimalMenuController implements Initializable {
     @FXML
     void onActionDisplayMainMenu(ActionEvent event) throws IOException {
 
-        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "This will clear all text field values, do you want to contine?");
+        alert.setTitle("CONFIRMATION");
+
+        Optional<ButtonType> result = alert.showAndWait();
+
+        if(result.isPresent() && result.get() == ButtonType.OK) {
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        }
 
     }
 
     @FXML
     void onActionSaveAnimal(ActionEvent event) throws IOException {
 
-        int id = Integer.parseInt(animalIdTxt.getText());
-        String breed = breedTxt.getText();
-        int lifespan = Integer.parseInt(lifespanTxt.getText());
-        String behavior = behaviorTxt.getText();
-        double price = Double.parseDouble(priceTxt.getText());
-        boolean isVaccinated;
-        String special = null;
+        try {
+            int id = Integer.parseInt(animalIdTxt.getText());
+            String breed = breedTxt.getText();
+            int lifespan = Integer.parseInt(lifespanTxt.getText());
+            String behavior = behaviorTxt.getText();
+            double price = Double.parseDouble(priceTxt.getText());
+            boolean isVaccinated;
+            String special = null;
 
-        if(vaccYesRBtn.isSelected()) {
-            isVaccinated = true;
-        } else {
-            isVaccinated = false;
+            if (vaccYesRBtn.isSelected()) {
+                isVaccinated = true;
+            } else {
+                isVaccinated = false;
+            }
+
+            DataProvider.addAnimal(new Dog(id, breed, lifespan, behavior, price, isVaccinated, special));
+
+            stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+        } catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Dialog");
+            alert.setContentText("Please enter valid value for each text field.");
+            alert.showAndWait();
         }
-
-        DataProvider.addAnimal(new Dog(id, breed, lifespan, behavior, price, isVaccinated, special));
-
-        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
-        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
-        stage.setScene(new Scene(scene));
-        stage.show();
 
     }
 
